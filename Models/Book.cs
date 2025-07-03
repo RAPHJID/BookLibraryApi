f8a0934108af5e2029b9c8ed06fe6f679e488fab
jidraf <Kimachiajidraf@gmail.com>
2025-07-03 13:59:10 +0300

    Model validation required

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,8 +3,13 @@ namespace BookLibraryApi.Models
     public class Book
     {
         public int Id {get;set;}
+        [Required(ErrorMessage = "Title required")]
+        [MaxLength(20 , ErrorMessage ="Cannot be more than 20 characters")]
         public string Title {get;set;}
+        [Required(ErrorMessage = "Date of publish required")]
         public DateTime PublishedDate {get;set;}
+        [Required(ErrorMessage = "Description required")]
+        [MaxLength(50, ErrorMessage = "Description cannot have more than 0 characters")]
         public string Description {get;set;}
         public bool IsAvailable {get;set;} = true;
 
